d6265c30cf69c785d3af4f453a78310bdd3f5a99
Thibaud Ros <thibaud.ros@gmail.com>
2021-12-27 17:56:29 +0100

    création controllers sauce

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,33 @@
+const Sauce = require('../models/Sauce');
+
+exports.createSauce = (req, res, next) => {
+    const sauce = new Sauce({
+        ...req.body
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.modifySauce = (req, res, next) => {
+    Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.deleteOneSauce = (req, res, next) => {
+    Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
+}
+
+exports.showOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+}
+exports.showAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }));
+}
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,36 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Sauce = require('../models/Sauce');
+const sauceCtrl = require('../controllers/sauce');
 
+router.get('/api/sauces/:id', sauceCtrl.showOneSauce);
+router.put('/api/sauces/:id', sauceCtrl.modifySauce);
 
-router.get('/api/sauces/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
-router.put('/api/sauces/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-router.delete('/api/sauces/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-router.get('/api/sauces', (req, res, next) => {
-    Sauce.find()
-        .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
-});
+router.delete('/api/sauces/:id', sauceCtrl.deleteOneSauce);
 
-router.post('/', (req, res, next) => {
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
-        .catch(error => res.status(400).json({ error }));
-});
+router.get('/api/sauces', sauceCtrl.showAllSauces);
+
+router.post('/api/sauces', sauceCtrl.createSauce);
 
 module.exports = router;
